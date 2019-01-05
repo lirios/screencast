010eb52fe31969251667ef448e1ba6c47164a3fd
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2019-01-06 00:16:00 +0100

    Load translations

--- src/screencast/main.cpp
+++ src/screencast/main.cpp
@@ -35,6 +35,45 @@
 
 #define TR(x) QT_TRANSLATE_NOOP("Command line parser", QLatin1String(x))
 
+static void loadQtTranslations()
+{
+#ifndef QT_NO_TRANSLATION
+    QString locale = QLocale::system().name();
+
+    // Load Qt translations
+    QTranslator *qtTranslator = new QTranslator(QCoreApplication::instance());
+    if (qtTranslator->load(QStringLiteral("qt_%1").arg(locale), QLibraryInfo::location(QLibraryInfo::TranslationsPath))) {
+        qApp->installTranslator(qtTranslator);
+    } else {
+        delete qtTranslator;
+    }
+#endif
+}
+
+static void loadAppTranslations()
+{
+#ifndef QT_NO_TRANSLATION
+    QString locale = QLocale::system().name();
+
+    // Find the translations directory
+    const QString path = QLatin1String("liri-screencast/translations");
+    const QString translationsDir =
+        QStandardPaths::locate(QStandardPaths::GenericDataLocation,
+                               path,
+                               QStandardPaths::LocateDirectory);
+
+    // Load shell translations
+    QTranslator *appTranslator = new QTranslator(QCoreApplication::instance());
+    if (appTranslator->load(QStringLiteral("%1/screencast_%3").arg(translationsDir, locale))) {
+        QCoreApplication::installTranslator(appTranslator);
+    } else if (locale == QLatin1String("C") ||
+                locale.startsWith(QLatin1String("en"))) {
+        // English is the default, it's translated anyway
+        delete appTranslator;
+    }
+#endif
+}
+
 int main(int argc, char *argv[])
 {
     // Setup application
@@ -43,6 +82,10 @@ int main(int argc, char *argv[])
     app.setApplicationVersion(QLatin1String(LIRISCREENCAST_VERSION));
     app.setOrganizationDomain(QLatin1String("liri.io"));
     app.setOrganizationName(QLatin1String("Liri"));
+
+    // Load translations
+    loadQtTranslations();
+    loadAppTranslations();
 
     // Command line parser
     QCommandLineParser parser;
