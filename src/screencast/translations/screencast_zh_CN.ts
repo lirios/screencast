a18f185c6f2055eded6bca9b40054da9b5427154
Liri CI <ci@liri.io>
2019-08-27 02:45:41 +0000

    Automatic merge of Transifex translations
    [ci skip]

--- src/screencast/translations/screencast_zh_CN.ts
+++ src/screencast/translations/screencast_zh_CN.ts
@@ -4,7 +4,7 @@
     <message>
         <location filename="../screencast.cpp" line="99"/>
         <source>Screencast from %1</source>
-        <translation>从%1录屏</translation>
+        <translation>从 %1 录屏</translation>
     </message>
 </context>
 </TS>
