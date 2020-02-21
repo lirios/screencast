83e2d646c3904918414b42cc99de1bd2aaca610d
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2020-02-21 06:05:00 +0100

    Add SPDX License markers
    Convert license statements to SPDX markers and
    add license files as required by REUSE specification.
    
    See https://reuse.software/faq/

--- cmake/FindGStreamer.cmake
+++ cmake/FindGStreamer.cmake
@@ -1,2 +1,8 @@
+#
+# SPDX-FileCopyrightText: 2020 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
 find_package(PkgConfig)
 pkg_check_modules(GStreamer gstreamer-1.0 REQUIRED IMPORTED_TARGET)
