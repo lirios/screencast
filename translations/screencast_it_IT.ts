dfb68543c6ae48f32b45752ab8992a82fe679dcf
Liri CI <ci@liri.io>
2020-05-13 00:18:13 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ translations/screencast_it_IT.ts
@@ -0,0 +1,10 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="it_IT" version="2.1">
+<context>
+    <name>Screencast</name>
+    <message>
+        <location filename="../src/screencast/screencast.cpp" line="121"/>
+        <source>Screencast from %1</source>
+        <translation>Registra schermo da %1</translation>
+    </message>
+</context>
+</TS>
